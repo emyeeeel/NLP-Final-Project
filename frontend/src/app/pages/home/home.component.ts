076d9bb48de2d47dff2afdaea649a4c4e946646c
Emyeeeel <maryamielcatado@gmail.com>
2025-05-28 01:04:40 +0800

    added dashboard/home content with some stylings

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ImageInputComponent } from "../../componenets/image-input/image-input.component";
 
 @Component({
   selector: 'app-home',
-  imports: [ImageInputComponent],
+  imports: [CommonModule, ImageInputComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  isSidebarHidden = false;
 
+  toggleSidebar() {
+    this.isSidebarHidden = !this.isSidebarHidden;
+  }
 }
